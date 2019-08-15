b2831eb1f0ff0a825e0cf50d9a622e1108a99c6e
Nanozuki Crows <nanozuki.crows@gmail.com>
2019-08-15 10:10:52 +0800

    fixed bug for OID.GetBson

--- oid.go
+++ oid.go
@@ -8,16 +8,20 @@ import (
 	"github.com/vmihailenco/msgpack"
 )
 
+// OID fit for json, bson and msgpack
 type OID bson.ObjectId
 
+// String of OID
 func (o OID) String() string {
 	return bson.ObjectId(o).Hex()
 }
 
+// ObjectID value of OID
 func (o OID) ObjectID() bson.ObjectId {
 	return bson.ObjectId(o)
 }
 
+// ParseOIDFromString parse oid from string
 func ParseOIDFromString(s string) (OID, error) {
 	if s == "" {
 		return OID(s), nil
@@ -30,11 +34,13 @@ func ParseOIDFromString(s string) (OID, error) {
 
 // BSON
 
-func (o *OID) GetBSON() (interface{}, error) {
+// GetBSON for saving to mongodb
+func (o OID) GetBSON() (interface{}, error) {
 	s := o.ObjectID()
 	return s, nil
 }
 
+// SetBSON for getting from mongodb
 func (o *OID) SetBSON(raw bson.Raw) error {
 	var bid bson.ObjectId
 	if err := raw.Unmarshal(&bid); err != nil {
@@ -46,6 +52,7 @@ func (o *OID) SetBSON(raw bson.Raw) error {
 
 // JSON
 
+// UnmarshalJSON unmarshal oid from json
 func (o *OID) UnmarshalJSON(b []byte) error {
 	var s string
 	if err := json.Unmarshal(b, &s); err != nil {
@@ -59,6 +66,7 @@ func (o *OID) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// MarshalJSON marshal oid to json
 func (o OID) MarshalJSON() ([]byte, error) {
 	s := bson.ObjectId(o).Hex()
 	return json.Marshal(s)
@@ -66,11 +74,13 @@ func (o OID) MarshalJSON() ([]byte, error) {
 
 // Msgpack
 
+// EncodeMsgpack encode oid to Msgpack
 func (o *OID) EncodeMsgpack(enc *msgpack.Encoder) error {
 	s := o.ObjectID().Hex()
 	return enc.Encode(s)
 }
 
+// DecodeMsgpack decode oid from Msgpack
 func (o *OID) DecodeMsgpack(dec *msgpack.Decoder) error {
 	var s string
 	if err := dec.Decode(&s); err != nil {
